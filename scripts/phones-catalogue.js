e290f0c899feb717276a972dfe1d499bfc266d57
mgrinko <glukym@gmail.com>
2018-03-01 20:37:35 +0200

    Custom event was added to catalogue

--- scripts/phones-catalogue.js
+++ scripts/phones-catalogue.js
@@ -1,11 +1,13 @@
 'use strict';
 
 export default class PhonesCatalogue {
-  constructor({ element, phones }) {
+  constructor({ element, phones,  }) {
     this._element = element;
     this._phones = phones;
 
     this._render();
+
+    this._element.onclick = this._onPhoneItemClicked.bind(this);
   }
 
   _render() {
@@ -13,18 +15,36 @@ export default class PhonesCatalogue {
 
     for (let phone of this._phones) {
       itemsHtml += `
-        <li class="thumbnail">
+        <li class="thumbnail"
+            data-element="phone-item"
+            data-phone-id="${ phone.id }">
+            
           <a href="#!/phones/${ phone.id }" class="thumb">
             <img alt="${ phone.name }" src="${ phone.imageUrl }">
           </a>
-          <a href="#!/phones/motorola-xoom-with-wi-fi">${ phone.name }</a>
+          <a href="#!/phones/${ phone.id }">${ phone.name }</a>
           <p>${ phone.snippet }</p>
         </li>
       `;
     }
 
     this._element.innerHTML = `
+      <h2>Catalogue</h2>
       <ul class="phones">${ itemsHtml }</ul>
     `;
+  }
+
+  _onPhoneItemClicked(event) {
+    let phoneElement = event.target.closest('[data-element="phone-item"]');
+
+    if (!phoneElement) {
+      return;
+    }
+
+    let customEvent = new CustomEvent('phoneSelected', {
+      detail: phoneElement.dataset.phoneId,
+    });
+
+    this._element.dispatchEvent(customEvent);
   }
 }
--- scripts/phones-page.js
+++ scripts/phones-page.js
@@ -9,10 +9,20 @@ export default class PhonesPage {
 
     this._render();
 
+    // this._shoppingCart = new ShoppingCart({
+    //   element: document.querySelector('[data-component="shopping-cart"]'),
+    // });
+
     this._phonesCatalogue = new PhonesCatalogue({
       element: document.querySelector('[data-component="phones-catalogue"]'),
       phones: PhonesService.getPhones(),
     });
+
+    this._phonesCatalogue._element.addEventListener('phoneSelected', (event) => {
+      alert(event.detail);
+    });
+
+
   }
 
   _render() {
