b053cfc67079ffcdb8db66384af0cd351ad2409c
muturgan <caxap_8787@mail.ru>
2018-03-04 02:16:32 +0300

    добавил ссылку для просмотра работы страницы (на всякий случай)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 ﻿"use strict"
+/*
+т.к. у меня не получается посмотреть работу страницы через gh-pages,
+то я залил то же самое себе в мастер в репозиторий muturgan.github.io
+
+работу страницы можно посмотреть по ссылке:
+https://muturgan.github.io/my_learning_project
+*/
 
 let phonesFilterControl = new PhonesFilterControl(document.body.querySelector(`.controls`));
 
