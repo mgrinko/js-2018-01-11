48a908949e4e5774ed07518a62fd3f780be09222
SHAP <shirling2@mail.ru>
2018-03-05 01:34:20 +0500

    deleted extra text

--- scripts/phones-page.js
+++ scripts/phones-page.js
@@ -3,8 +3,6 @@
 import PhonesCatalogue from './phones-catalogue.js';
 import PhonesService from './phones-service.js';
 import ShoppingCart from './shopping-cart.js';
-// import PhonesSearch from './phones-search.js';
-// import PhonesSort from './phones-sort.js';
 
 export default class PhonesPage {
   constructor({ element }) {
@@ -17,19 +15,6 @@ export default class PhonesPage {
       element: document.querySelector('[data-component="shopping-cart"]'),
       items: [1, 2, 3]
     });
-
-    // Phones sort initialize
-    // this._phonesSort = new PhonesSort({
-    //   element: document.querySelector('[data-component="phones-sort"]'),
-    //   phones: PhonesService.getPhones()
-    // });
-
-    // Sort if selected
-    // this._phonesSort.on('selectChangeBegin', event => {
-    //   let phonesSort = event.detail;
-    //
-    //   this._phonesCatalogue.render(phonesSort);
-    // });
 
     // Phones catalogue initialize
     this._phonesCatalogue = new PhonesCatalogue({
@@ -45,33 +30,6 @@ export default class PhonesPage {
 
       this._shoppingCart.addItem(phoneId);
     });
-
-    // Phones search initialize
-    // this._phonesSearch = new PhonesSearch({
-    //   element: document.querySelector('[data-component="phones-search"]'),
-    //   phones: this._phonesSort.getDefaultSort()
-    // });
-
-    // if have search phone
-    // this._phonesSearch.on('phonesSearch', event => {
-    //   let phonesSearch = event.detail;
-    //
-    //   this._phonesCatalogue.render(phonesSearch);
-    // });
-
-    // not found search phone
-    // this._phonesSearch.on('phonesSearchNotFound', event => {
-    //   let message = event.detail;
-    //
-    //   this._phonesCatalogue.notification(message);
-    // });
-
-    // default phones list
-    // this._phonesSearch.on('phonesDefault', event => {
-    //   let phones = event.detail;
-    //
-    //   this._phonesCatalogue.render(phones);
-    // });
   }
 
   _render() {
